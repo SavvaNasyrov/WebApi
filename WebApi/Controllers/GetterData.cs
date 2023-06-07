683c7313f7321ccfc836da3140418e22005c2df1
Савва Насыров <vasyanasyrov06288@gmail.com>
2023-06-07 21:54:53 +0500

    #Added getterData

--- WebApi/Controllers/GetterData.cs
+++ WebApi/Controllers/GetterData.cs
@@ -1,4 +1,6 @@
-﻿namespace WebApi.Controllers
+﻿using System.Text.Json;
+
+namespace WebApi.Controllers
 {
     public class GetterData
     {
@@ -11,6 +13,21 @@
             client = new HttpClient();
             ACSESS_TOKEN = File.ReadAllText("ACSESS_TOKEN.txt");
             Version = "5.131";
+        }
+
+        async Task<PostsNamespace.VKUsersPosts> GetLast5Posts(string ID)
+        {
+            string? result = "";
+
+            HttpResponseMessage response = await client.GetAsync($"https://api.vk.com/method/wall.get?owner_id={ID}&count=5&lang=ru&access_token={ACSESS_TOKEN}&v={Version}");
+            if (response.IsSuccessStatusCode)
+            {
+                result = await response.Content.ReadAsStringAsync();
+            }
+
+            PostsNamespace.VKUsersPosts? posts = JsonSerializer.Deserialize<PostsNamespace.VKUsersPosts>(result, PostsNamespace.SerContext.Default.VKUsersPosts);
+
+            return posts;
         }
     }
 }
