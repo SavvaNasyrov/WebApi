40a861cf9cde18b293ad50b3b1aa9706ee9b625e
Савва Насыров <vasyanasyrov06288@gmail.com>
2023-06-17 18:02:18 +0500

    #Realease

--- WebApi/Controllers/HomeController.cs
+++ WebApi/Controllers/HomeController.cs
@@ -38,23 +38,22 @@ namespace WebApi.Controllers
         [HttpGet("{owner_id}")]
         public async Task< IActionResult> JsonString(string owner_id) 
         {
-            GetterData getter = new GetterData();
-            VKUsersPosts posts = await getter.GetLast5Posts(owner_id);
-
-            OccurrenceСounter сounter = new(new(webApiContext));
-
-            OccurrencesData dicts = await сounter.CollectOccurrences(posts);
 
             try
             {
-                return Ok("ok");
+                GetterData getter = new GetterData();
+                VKUsersPosts posts = await getter.GetLast5Posts(owner_id);
+
+                OccurrenceСounter сounter = new(new(webApiContext));
+
+                OccurrencesData dicts = await сounter.CollectOccurrences(posts);
             }
             catch 
             {
                 return BadRequest();
             }
 
-            
+            return Ok("ok");
         }
     }
 }
